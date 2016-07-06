bdb477137fcef37886f6b1ca08eb0f57c85e2c99
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-07-05 23:12:15 -0600

    cleaned up js

--- js/script.js
+++ js/script.js
@@ -25,8 +25,7 @@ $(document).ready(function() {
 			{ numCatClicks: 0, catName: 'jetske', catURL: "https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454", nickname: ['lucky', 'bro']},
 			{ numCatClicks: 0, catName: 'larry', catURL: "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496", nickname: ['lucky', 'bro']},
 			{ numCatClicks: 0, catName: 'lussy', catURL: "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426", nickname: ['lucky', 'bro']}
-		],
-		currentCat: 0
+		]
 	};
 
 	var VeiwModel = function() {
@@ -51,92 +50,4 @@ $(document).ready(function() {
 
 	ko.applyBindings(new VeiwModel());
 
-	var view = {
-		init: function() {
-			$('#catNames').on('click', 'p', function(){
-				controller.clickCatName($(this).html());
-			});
-
-			$('#displayedCat').on('click', 'img', function() {
-				controller.clickCatImg($(this).parent().children('#catName').html());
-			});
-
-			$('#admin-button').click(function() {
-				$('#admin-area').show();
-				controller.adminButtonClick();
-			});
-
-			$('#admin-area').submit(function() {
-				event.preventDefault();
-				var catName = $('#admin-catName').val(); 
-				var URL = $('#admin-URL').val(); 
-				var numCatClicks = $('#admin-numCatClicks').val(); 
-				controller.changeCatDetails(catName, URL, numCatClicks);
-				$('#admin-area').hide();
-			})
-			.on('click', '#admin-cancel', function() {
-				$('#admin-area').hide();
-			});
-		},
-
-		addCats: function(cats) {
-			for (var i = 0; i < cats.length; i++) {
-				$('#catNames').append('<li><p>' + cats[i].catName + '</p></li>');
-			}
-		},
-
-		numCatClicks: function(numCatClicks) {
-			$('#displayedCat').children('#numCatClicks').text(numCatClicks);
-		},
-
-		showCurrentCatVals: function(currentCat) {
-			$('#admin-catName').val(currentCat.catName); 
-			$('#admin-URL').val(currentCat.catURL); 
-			$('#admin-numCatClicks').val(currentCat.numCatClicks); 
-		},
-
-		generateCat: function(cat) {
-			$('#displayedCat').empty().append(
-				'<p id="numCatClicks">' + cat.numCatClicks + '</p>'+
-			    '<p id="catName">' + cat.catName + '</p>'+
-			    '<img src=' + cat.catURL + ' alt="' + cat.catURL + '">'
-		    );
-
-		}
-	};
-
-	var controller = {
-		init: function() {
-			view.init();
-			view.addCats(model.listOfCats);
-			view.generateCat(model.listOfCats[model.currentCat]);			
-		},
-
-		clickCatName: function(catName) {
-			for (var i = 0; i < model.listOfCats.length; i++) {
-				if (model.listOfCats[i].catName == catName) {
-					view.generateCat(model.listOfCats[i]);
-					model.currentCat = i;
-				}
-			}
-		},
-
-		clickCatImg: function(catName) {
-			model.listOfCats[model.currentCat].numCatClicks++;
-			view.numCatClicks(model.listOfCats[model.currentCat].numCatClicks)
-		},
-
-		adminButtonClick: function() {
-			view.showCurrentCatVals(model.listOfCats[model.currentCat]);
-		},
-
-		changeCatDetails: function(catName, URL, numCatClicks) {
-			model.listOfCats[model.currentCat].catName = catName;
-			model.listOfCats[model.currentCat].catURL = URL;
-			model.listOfCats[model.currentCat].numCatClicks = numCatClicks;
-			view.generateCat(model.listOfCats[model.currentCat]);
-		}
-	};
-
-	// controller.init();
 });
